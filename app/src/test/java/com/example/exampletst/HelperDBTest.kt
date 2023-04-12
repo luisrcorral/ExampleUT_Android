b1b04d4a51ea87fe2907f96654681212c937eeb8
LuisCorral <luisrcorral@gmail.com>
2023-04-12 16:50:07 -0600

    Add new test case TV2

--- app/src/test/java/com/example/exampletst/HelperDBTest.kt
+++ app/src/test/java/com/example/exampletst/HelperDBTest.kt
@@ -2,6 +2,7 @@ package com.example.exampletst
 
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class HelperDBTest {
@@ -13,7 +14,7 @@ class HelperDBTest {
         val helperClassInstance = HelperDB()
 
         // Establish Input Values
-        val expectedString: String = "Hello" // I'm setting a different value on purpose
+        val expectedString: String = "HelloW" // I'm setting a different value on purpose
         var actualString : String = "xxxxxx"
 
         // Execute the method under test
@@ -25,4 +26,27 @@ class HelperDBTest {
 
         // To run the test, right-click on the exampletst folder and go to run test
     }
+
+    @Test
+    fun testMyFun_TV2() {
+        // Instantiate the class under test
+        val helperClassInstance = HelperDB()
+
+        // Establish Input Values
+        val expectedString: String = "Hello World..." // I'm setting a different value on purpose
+        var actualString : String = "xxxxxx"
+
+        // Execute the method under test
+
+        actualString = helperClassInstance.myFun("Hello World...")
+
+        // Evaluate your assertion
+
+        //assertEquals(actualString, expectedString)
+        assertNotNull(expectedString)
+
+        // To run the test, right-click on the exampletst folder and go to run test
+    }
+
+
 }
